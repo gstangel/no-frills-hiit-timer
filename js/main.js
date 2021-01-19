a5aee2f46e87d611f0e2572cc05fa910527e4c52
gabe <gabestangel70@gmail.com>
2021-01-19 10:15:02 -0600

    cleaned up code

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
+// disclaimer: this is one of my first independant coding projects.  This
+// code is shotty to say the least
+
 //point to slider elements on html
 var lowSecSlider = document.getElementById("low-sec-slider");
 var lowSecLabel = document.getElementById("low-sec-label");
 
 //logic to convert from seconds to min and sec
-lowSecSlider.oninput = function() {
-  if(lowSecSlider.value >= 60)
-  {
+lowSecSlider.oninput = function () {
+  if (lowSecSlider.value >= 60) {
     var min = Math.floor(lowSecSlider.value / 60);
     var seconds = lowSecSlider.value - min * 60;
     lowSecLabel.innerHTML = min + " minutes" + " " + seconds + " seconds";
   }
   else {
-    lowSecLabel.innerHTML = lowSecSlider.value + " seconds"
+    lowSecLabel.innerHTML = lowSecSlider.value + " seconds";
   }
-
 }
 
 //update high sec slider
 var highSecSlider = document.getElementById("high-sec-slider");
 var highSecLabel = document.getElementById("high-sec-label");
 
-//logic to convert from seconds to min and sec for high sec slider
-highSecSlider.oninput = function() {
-  if(highSecSlider.value >= 60)
-  {
+//logic to convert from seconds, to min and sec, for high sec slider
+highSecSlider.oninput = function () {
+  if (highSecSlider.value >= 60) {
     var min = Math.floor(highSecSlider.value / 60);
     var seconds = highSecSlider.value - min * 60;
     highSecLabel.innerHTML = min + " minutes" + " " + seconds + " seconds";
@@ -31,116 +31,104 @@ highSecSlider.oninput = function() {
   else {
     highSecLabel.innerHTML = highSecSlider.value + " seconds"
   }
-
 }
 
 //update rounds slider
 var roundsSlider = document.getElementById("rounds-slider");
 var roundsLabel = document.getElementById("rounds-label");
 
-
-roundsSlider.oninput = function() {
+roundsSlider.oninput = function () {
   roundsLabel.innerHTML = this.value;
-//for audio display
+  //for audio playback when round starts or ends
 
 }
-//audio effects
+
+//loading audio snippits into memory
 let starting321 = new Audio("starting321.mp3");
 let highIntensity = new Audio("high-intensity.mp3");
 let lowIntensity = new Audio("low-intensity.mp3");
 let workoutComplete = new Audio("workout-complete.mp3");
+
 //fuction to control timer logic as well as play audio effects
 function startTimer() {
-//point to html elements
-  	let lowIntensitySec = parseInt(document.getElementById("low-sec-slider").value);
-  	let highIntensitySec = parseInt(document.getElementById("high-sec-slider").value);
-    let rounds = parseInt(document.getElementById("rounds-slider").value);
-    var lowIntensityVar = lowIntensitySec;
-    var highIntensityVar = highIntensitySec;
-    //bool to determine which cycle
-    var isLow = true;
-    //counters for intervals
-    var soundPlayed = false;
-    var roundsCounter = 0;
-    var lowCounter = 0;
-    var highCounter = 0;
-    //timer update seconds
-    let interval = 0.01
+  //point to html elements
+  let lowIntensitySec = parseInt(document.getElementById("low-sec-slider").value);
+  let highIntensitySec = parseInt(document.getElementById("high-sec-slider").value);
+  let roundInput = parseInt(document.getElementById("rounds-slider").value);
+  var lowIntensityVar = lowIntensitySec;
+  var highIntensityVar = highIntensitySec;
+  //bool to determine which cycle is active
+  var isLow = true;
+  //counters for intervals
+  var soundPlayed = false;
+  var roundsCounter = 0;
+  var lowCounter = 0;
+  var highCounter = 0;
+  //timer update seconds
+  let interval = 0.01
 
-    function updateTimer()
-    {
-      document.getElementById("rounds-left").innerHTML="Rounds Remaining: " +(rounds-roundsCounter);
-      //logic to determine if rounds have been completed
-      if(roundsCounter < rounds){
-        if(isLow == true)
-        {
-          if(soundPlayed == false)
-          {
-            soundPlayed = true;
-            lowIntensity.play();
-
-          }
-
-          document.getElementById("intensity").style.color = "green";
-          document.getElementById("countdown").style.borderColor = "green";
-          document.getElementById("intensity").innerHTML="REST";
-          if(lowCounter<lowIntensityVar)
-          {
-            document.getElementById("countdown").innerHTML=lowCounter.toFixed(2); + " seconds";
-            lowCounter += interval;
-          }
-          else {
-            soundPlayed = false;
-            isLow = false;
-            lowCounter = 0;
-          }
+  function updateTimer() {
+    document.getElementById("rounds-left").innerHTML = "Rounds Remaining: " + (roundInput - roundsCounter);
+    //logic to determine if rounds have been completed
+    if (roundsCounter < roundInput) {
+      if (isLow == true) {
+        if (soundPlayed == false) {
+          soundPlayed = true;
+          lowIntensity.play();
         }
-        else
-        {
-          //play high intensity sound
-          if(soundPlayed == false)
-          {
-            soundPlayed = true;
-            highIntensity.play();
-
-          }
-          //update html elements
-          document.getElementById("intensity").style.color = "red";
-          document.getElementById("countdown").style.borderColor = "red";
-          document.getElementById("intensity").innerHTML="WORK";
-          //logic for timer
-          if(highCounter<highIntensityVar)
-          {
-            document.getElementById("countdown").innerHTML=highCounter.toFixed(2); + "high";
-            highCounter += interval;
-          }
-          //loop back to low intensity
-          else {
-            soundPlayed = false;
-            highCounter = 0;
-            isLow = true;
-            roundsCounter ++;
-          }
+        //update styling to reflect active intensity
+        document.getElementById("intensity").style.color = "green";
+        document.getElementById("countdown").style.borderColor = "green";
+        document.getElementById("intensity").innerHTML = "REST";
+        //when there is still time left in the current low intensity round, reflect that to the user
+        if (lowCounter < lowIntensityVar) {
+          document.getElementById("countdown").innerHTML = lowCounter.toFixed(2); + " seconds";
+          lowCounter += interval;
         }
+        else {
+          soundPlayed = false;
+          isLow = false;
+          lowCounter = 0;
+        }
+      }
+      else {
+        //play high intensity sound when round intensity changes
+        if (soundPlayed == false) {
+          soundPlayed = true;
+          highIntensity.play();
+        }
+        //update html elements
+        document.getElementById("intensity").style.color = "red";
+        document.getElementById("countdown").style.borderColor = "red";
+        document.getElementById("intensity").innerHTML = "WORK";
+        //logic for timer
+        if (highCounter < highIntensityVar) {
+          document.getElementById("countdown").innerHTML = highCounter.toFixed(2); + "high";
+          highCounter += interval;
+        }
+        //loop back to low intensity
+        else {
+          soundPlayed = false;
+          highCounter = 0;
+          isLow = true;
+          roundsCounter++;
+        }
+      }
     }
     else {
-      if(soundPlayed == false)
-      {
+      if (soundPlayed == false) {
         workoutComplete.play();
         soundPlayed = true;
       }
-
     }
   }
-  function startTimer()
-  {
-    setInterval(updateTimer,10);
+  //call the function every 10ms to go through logic flow for the timer
+  function startTimer() {
+    setInterval(updateTimer, 10);
   }
 
   //play starting sound
   starting321.play();
   //timeout for starting sound to complete
   setTimeout(startTimer, 4500);
-
-
 }
